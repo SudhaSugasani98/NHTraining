fc2826eb991022204640d9f048c1c43a6cce77fc
SudhaSugasani <sudhasugasani@98.com>
2022-01-25 15:48:49 +0530

    first submission

---
+++ Day1Project3/Day1Project3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fn, sn;
+            int p = 1;
+
+            Console.WriteLine("Enter First Number");
+            fn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            sn = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= sn; i++)
+                p = p * fn;
+            
+            Console.WriteLine("Power = " + p);
+            Console.ReadLine();
+        }
+    }
+}
