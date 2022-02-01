13acf690479fafb7713d5b51e4f2d24af9ddd762
SudhaSugasani <sudhasugasani@98.com>
2022-02-01 14:35:35 +0530

    employee class with 3 public variables

---
+++ Day7 Morning Assignment/Day7project3/Day7project3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7project3
+{
+    class Employee
+    {
+        public int id;
+        public string name;
+        public int salary;
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*********************************************************************************************************
+             * Author:Sudha Sugasani
+             * Purpose:Creating Employee Class with 3public variables
+             **********************************************************************************************************/
+            Employee emp = new Employee() { id = 1, name = "sudha", salary = 5000 };
+            Console.WriteLine($"Id={emp.id},Name={emp.name},Salary={emp.salary}");
+            Console.ReadLine();
+        }
+    }
+}
