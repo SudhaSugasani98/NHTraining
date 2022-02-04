9a79a6362ad0090f03f51d1b44d3fd17a82e0208
SudhaSugasani <sudhasugasani@98.com>
2022-02-04 15:54:26 +0530

    Example for Method Overriding using new keyword

---
+++ Day 10 Morning Assignment/Day10project4/Day10project4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10project4
+{
+    
+        /*****************************************************
+         * Author:Sudha Sugasani
+         * Purpose:Example code for Method Overriding        
+         *****************************************************/
+
+        class EnglishMessage
+        {
+            /// <summary>
+            /// This method will print Hi message
+            /// </summary>
+           
+            public void PrintHi()
+            {
+            Console.WriteLine("Hi");
+            }
+
+            /// <summary>
+            /// This method will print Hello Message
+            /// </summary>
+            
+
+            public void PrintHello()
+            {
+            Console.WriteLine("Hello");
+            }
+
+
+            /// <summary>
+            /// This method will print Good Morning message
+            /// </summary>
+            public void PrintGM()
+            {
+            Console.WriteLine("Good Morning");
+            }
+        }
+        class TeluguMessage:EnglishMessage
+        {
+            /// <summary>
+            /// This method will print Subhodhayam message
+            /// </summary>
+            public new void  PrintGM()
+            {
+            Console.WriteLine("Subhodhayam");
+            }
+        }
+            
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                TeluguMessage m1 = new TeluguMessage();
+                m1.PrintHi();
+                m1.PrintHello();
+                m1.PrintGM();
+                
+                Console.ReadLine();
+
+            }
+        }
+
+
+    }
