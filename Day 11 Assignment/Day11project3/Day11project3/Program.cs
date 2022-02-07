446f9271e0c4c9917bd986e1268fe098641072a5
SudhaSugasani <sudhasugasani@98.com>
2022-02-07 18:03:13 +0530

    creating a class with properties to access private variables

---
+++ Day 11 Assignment/Day11project3/Day11project3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11project3
+{
+    /*****************************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Creating a claass with properties to access private
+     *         Variables.
+     *****************************************************************/
+    class Employe
+    {
+        private int id;
+        private string name;
+        private string designation;
+        private int salary;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name 
+        { 
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Designation
+        {
+            get { return designation; }
+            set { designation = value; }
+        }
+        public int Salary
+        {
+            get
+            {
+                salary = (designation == "S") ? 30000 : 60000;
+                return salary;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe emp = new Employe();
+            emp.Id = 505;
+            Console.WriteLine($"Employee ID is {emp.Id}");
+            emp.Name = "Sudha";
+            Console.WriteLine($"Employe Name is {emp.Name}");
+            
+            emp.Designation = "S";
+            Console.WriteLine(emp.Salary);
+            Console.ReadLine();
+        }
+    }
+}
