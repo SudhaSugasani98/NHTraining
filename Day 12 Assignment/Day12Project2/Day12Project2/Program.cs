238031f5ea807cfffff9dfb395bb2c09f532a738
SudhaSugasani <sudhasugasani@98.com>
2022-02-08 22:00:27 +0530

    Program to show the use of finally block

---
+++ Day 12 Assignment/Day12Project2/Day12Project2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Activities.Expressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Project2
+{
+   
+
+    internal class Program
+    {
+        /*******************************************************
+         * Author:Sudha Sugasani
+         * Purpose:Program to show the use of finally block
+         * *****************************************************/
+
+        static void Main(String[] args)
+        {
+             try
+             {
+                 int a = 100;
+                 int b = 5;
+                 int c = a / b;
+                 Console.WriteLine($"Division of two numbers is {c}");
+             }
+             catch(Exception)
+             {
+
+             }
+             finally
+             {
+                 Console.WriteLine("\n\n\n\n\n Designed by Sudha Sugasani");
+                 Console.ReadLine();
+             }
+
+
+
+
+
+        }
+        
+    }
+}
