46e9d7727919497c3f2fe7feb69f245f9f0fe155
SudhaSugasani <sudhasugasani@98.com>
2022-02-09 12:37:49 +0530

    Program to initialize and print values of jagged array using char

---
+++ Day 13 Assignment/Day13project8/Day13project8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13project8
+{
+        /******************************************************
+         * Author:Sudha Kumari Sugasani
+         * Purpose:Declaration and printing of a jagged array
+         *         using character array
+         * ****************************************************/
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //Declaration and intialisation of jagged character array
+                char[][] data = new char[5][];
+                data[0] = new char[] { 's','u','d','h','a' };
+                data[1] = new char[] { 's', 'u', 'd', 'h', 'a','s' };
+                data[2] = new char[] { 's', 'u', 'd', 'h', 'a','s','u' };
+                data[3] = new char[] { 's', 'u', 'd', 'h', 'a','s','u','g' };
+                data[4] = new char[] { 's', 'u', 'd', 'h', 'a','s','u','g','a','s','a','n','i' };
+
+            //Printing of jagged character array values
+            for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < data[i].Length; j++)
+                    {
+                        Console.Write(data[i][j] + " ");
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
