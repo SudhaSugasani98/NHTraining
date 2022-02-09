ce5b1ab2b68c5cae09a910d589831d3be3c9b71b
SudhaSugasani <sudhasugasani@98.com>
2022-02-09 13:05:05 +0530

    Example program for Stack

---
+++ Day 13 Assignment/Day13Project10/Day13Project10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13Project10
+{
+    /**********************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Example program for stack
+     * ********************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> data = new Stack<int>();
+            data.Push(10);
+            data.Push(20);
+            data.Push(30);
+            Console.WriteLine($"Count before pop is {data.Count}");
+            Console.WriteLine($"Pop value is {data.Pop()}");
+            Console.WriteLine($"Count after pop is {data.Count}");
+            Console.WriteLine($"Count before peek is {data.Count}");
+            Console.WriteLine($"Peek value is {data.Peek()}");
+            Console.WriteLine($"Count after peek is {data.Count}");
+            Console.ReadLine();
+        }
+    }
+}
