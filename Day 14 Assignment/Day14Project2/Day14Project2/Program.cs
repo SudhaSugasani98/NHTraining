21710726cf022d037da7260d3c5138dd824d8b85
SudhaSugasani <sudhasugasani@98.com>
2022-02-10 11:37:55 +0530

    Example program for normal properties

---
+++ Day 14 Assignment/Day14Project2/Day14Project2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Project2
+{
+    /*************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Example program for normal properties
+     * ***********************************************/
+    class AverageSpeed
+    {
+        private int distance;
+        private int time;
+        private int speed;
+        public int Distance
+        {
+            set
+            {
+                distance = value;
+            }
+        }
+        public int Time
+        {
+            set { time = value; }
+        }
+        public int Speed
+        {
+            get
+            {
+                speed = distance / time;
+                return speed;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AverageSpeed avg = new AverageSpeed();
+            avg.Distance = 10;
+            avg.Time = 5;
+            Console.WriteLine($"Speed is {avg.Speed}");
+            Console.ReadLine();
+        }
+    }
+}
