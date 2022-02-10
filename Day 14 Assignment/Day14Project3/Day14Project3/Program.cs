b67d765ce0bca706ee4f3bb01c5a99a1e6ea7e05
SudhaSugasani <sudhasugasani@98.com>
2022-02-10 12:16:00 +0530

    Example program for auto-implemented properties

---
+++ Day 14 Assignment/Day14Project3/Day14Project3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Project3
+{
+   
+        /*********************************************************
+         * Author:Sudha Kumari Sugasani
+         * Purpose:Example program for auto-implemented properties
+         * *******************************************************/
+        class AverageVelocity
+        {
+            
+            public int Displacement
+            {
+            get
+            {
+                return 50;
+            }
+            }
+            public int Time
+            {
+                get{ return 20; }
+            }
+            public int Velocity
+            {
+            
+                get
+                {
+                   
+                    return Displacement/Time;
+                }
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                AverageVelocity vel=new AverageVelocity();
+                Console.WriteLine($"Displacement is { vel.Displacement}");
+                Console.WriteLine($"Time is {vel.Time}");
+                Console.WriteLine($"Velocity is {vel.Velocity}");
+                Console.ReadLine();
+            }
+        }
+    }
+
+  
