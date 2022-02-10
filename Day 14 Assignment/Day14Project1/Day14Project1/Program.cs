e910d5eb22cadf412ae7ffec16a5d30b7077b79b
SudhaSugasani <sudhasugasani@98.com>
2022-02-10 11:16:35 +0530

    Example program for sealed class

---
+++ Day 14 Assignment/Day14Project1/Day14Project1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Project1
+{
+    /***********************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Example for sealed class
+     **********************************/
+    sealed class Police
+    {
+        public static int Helpline = 100;
+        public string GetSecret()
+        {
+            return "Secret";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Police p = new Police();
+            Console.WriteLine(p.GetSecret());
+            Console.WriteLine(Police.Helpline);
+            Console.ReadLine();
+        }
+    }
+}
