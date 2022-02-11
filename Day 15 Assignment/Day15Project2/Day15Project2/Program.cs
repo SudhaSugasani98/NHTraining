bdbeb5bb3c728373046c108b7a83f5f661e5b543
SudhaSugasani <sudhasugasani@98.com>
2022-02-11 11:02:11 +0530

    Example program for File.WriteAllText()

---
+++ Day 15 Assignment/Day15Project2/Day15Project2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project2
+{
+   
+
+        /*************************************************
+         * Author:Sudha Kumari Sugasani
+         * Puropse:Example Program for File class using 
+         *         File.WriteAllText method
+         ************************************************/
+
+        class File2
+        {
+            /// <summary>
+            /// This method is used to Write mesage in the file
+            /// </summary>
+            public void WriteMessage()
+            {
+                //To wrie message file
+                File.WriteAllText("C:\\NH\\Files Using C#\\Hello.txt","Hi\nI'm learning C#");
+                Console.WriteLine("Message added sussfully");
+
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                File2 f2 = new File2();
+                f2.WriteMessage();
+                Console.ReadLine();
+            }
+        }
+    }
+
