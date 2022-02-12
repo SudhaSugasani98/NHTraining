f08027d64c229e793ef8b69d1bc24ac4e651b3b3
SudhaSugasani <sudhasugasani@98.com>
2022-02-12 17:04:58 +0530

    Example program for Files using Copy method

---
+++ Day 15 Assignment/Day15Project10/Day15Project10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project10
+{
+   
+
+        /**************************************************************
+         * Author:Sudha Kumari Sugasani
+         * Purpose:Example program for Files using Copy method
+         **************************************************************/
+        class Files10
+        {
+            /// <summary>
+            /// This method is used to Copy the files from one folder to another
+            /// </summary>
+            public void Copy1()
+            {
+                File.Copy("C:\\NH\\Files Using C#\\Hello.txt", "C:\\NH\\Copied files\\Hello.txt");
+                Console.WriteLine("File Copied Successfully");
+
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Files10 obj = new Files10();
+                obj.Copy1();
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+
+ 
