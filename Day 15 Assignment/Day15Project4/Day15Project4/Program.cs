9322355bf9d499066e2e8a84d0156d86b9756722
SudhaSugasani <sudhasugasani@98.com>
2022-02-12 12:46:49 +0530

    Example program for files using Delete method

---
+++ Day 15 Assignment/Day15Project4/Day15Project4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project4
+{
+    
+        /*************************************************
+         * Author:Sudha Kumari Sugasani
+         * Puropse:Example Program for File class using 
+         *         File.Delete method
+         ************************************************/
+
+        class File4
+        {
+            /// <summary>
+            /// This method is used to delete a file
+            /// </summary>
+            public void Delete()
+            {
+                //To delete file
+                File.Delete("C:\\NH\\Files Using C#\\Hello.txt");
+                Console.WriteLine("File deleted");
+
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                File4 f4 = new File4();
+                f4.Delete();
+                Console.ReadLine();
+            }
+        }
+    }
