99a97fd2d24a2205af5a7258890261c737df25b2
SudhaSugasani <sudhasugasani@98.com>
2022-02-12 13:42:29 +0530

    Example for files using GetCreationTime method

---
+++ Day 15 Assignment/Day15Project8/Day15Project8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project8
+{
+   
+        /*************************************************
+         * Author:Sudha Kumari Sugasani
+         * Puropse:Example Program for File class using 
+         *         File.GetCreationTime method
+         ************************************************/
+
+        class File8
+        {
+            /// <summary>
+            /// This method is used to see the creation time of the file
+            /// </summary>
+            public void Time()
+            {
+                //To see creation time
+                Console.WriteLine($"Creation time of the file is {File.GetCreationTime("C:\\NH\\Files Using C#\\Hello.txt")}");
+
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                File8 f8 = new File8();
+                f8.Time();
+                Console.ReadLine();
+            }
+        }
+    }
