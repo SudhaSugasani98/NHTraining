7ac7e6ae97517e5eb97649f09fab060f0529e22f
SudhaSugasani <sudhasugasani@98.com>
2022-02-14 11:52:47 +0530

    Creating a class library

---
+++ Day 16 Assignment/SudhaLibrary/Mathematics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudhaLibrary
+{
+    /*********************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Creating a class Library
+     * *******************************************************/
+    public class Mathematics
+    {
+        int input;
+        /// <summary>
+        ///This method will read input from user
+        /// </summary>
+        public void Readdata()
+        {
+            Console.WriteLine("Enter input");
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+        /// <summary>
+        /// This method will return factorial of a number
+        /// </summary>
+        /// <returns>factorial</returns>
+        public int GetFactorial()
+        {
+            int factorial = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+
+    }
+}
