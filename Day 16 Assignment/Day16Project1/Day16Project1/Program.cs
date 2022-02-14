474b1890c76380119dbafa1f242d8047d162cd76
SudhaSugasani <sudhasugasani@98.com>
2022-02-14 10:07:20 +0530

    Program to print Hello in Object Oriented Way

---
+++ Day 16 Assignment/Day16Project1/Day16Project1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16Project1
+{
+    /*******************************************************
+     *Author:Sudha Kumari Sugasani
+     *Purpose:Program to print Hello in object oriented way 
+     * *****************************************************/
+    class Hello
+    {
+        /// <summary>
+        /// This method is used to print Hello
+        /// </summary>
+        public static void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello.PrintHello();
+            Console.ReadLine();
+        }
+    }
+}
