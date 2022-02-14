faa6f1225d8da7d5ff59994d146eb013b669f6ad
SudhaSugasani <sudhasugasani@98.com>
2022-02-14 12:56:47 +0530

    Program to print Multiplication table of given number in Object Oriented way

---
+++ Day 16 Assignment/Day16Project4/Day16Project4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16Project4
+{
+    /***************************************************
+     *Author:Sudha Kumari Sugasani
+     *Purpose:Program to print multiplication table of 
+     *        a number in object oriented way.
+     * **************************************************/
+    class Table
+    {
+        public int input;
+        /// <summary>
+        /// This method is used to read data from user
+        /// </summary>
+        public void ReadData()
+        {
+            Console.WriteLine("Enter a number");
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+        /// <summary>
+        /// This method is used to print Multiplication table
+        /// </summary>
+        public  void GetMultiplicationTable()
+        {
+            Console.WriteLine($"Multiplication table of {input} is");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(input+"*"+i+"="+(input*i));
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Table t1 = new Table();
+            t1.ReadData();
+            t1.GetMultiplicationTable();
+            Console.ReadLine();
+        }
+    }
+}
