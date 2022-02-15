212e2e824467affb7900d85bc6e5935548f5d2c1
SudhaSugasani <sudhasugasani@98.com>
2022-02-15 12:06:17 +0530

    Example program to understand about Access Modifiers

---
+++ Day 17 Assignment/Day17Project1/PublicLibrary/MyPublicBaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudhaLibrary;
+
+namespace PublicLibrary
+{
+    public class MyDerivedClass:MyBaseClass
+    {
+        /// <summary>
+        /// Implementing Base class variables into Derivedclass in other library
+        /// </summary>
+        public void MyPublicBaseClassMethod()
+        {
+            a = 5;
+            b = 10;
+            c = 15;
+            d= 20;
+            e = 25;
+        }
+    }
+    public class MyPublicOtherClass
+    {
+        /// <summary>
+        /// Accessing base class variables in other class in other library
+        /// </summary>
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass mb = new MyBaseClass();
+            mb.a = 6;
+            mb.b = 5;
+            mb.c = 78;
+            mb.d = 55;
+            mb.e = 15;
+        }
+    }
+}
---
+++ Day 17 Assignment/Day17Project1/SudhaLibrary/MyBaseClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudhaLibrary
+{
+   /*********************************************************
+    * Author:Sudha Kumari Sugasani
+    * Purpose:Example to understand about Access Modifiers
+    * *******************************************************/
+
+    public class MyBaseClass
+    {
+        public int a ;
+        private int b ;
+        protected int c ;
+        internal int d;
+        protected internal int e ;
+        /// <summary>
+        /// Accessing variables in same class
+        /// </summary>
+        public void MyBaseClassMetod()
+        {
+            a = 5;
+            b = 19;
+            c = 7;
+            d = 5;
+            e = 15;
+        }
+    }
+    public class MyDerivedClass:MyBaseClass
+    {
+        /// <summary>
+        /// Accessing variabes in derived class in same library
+        /// </summary>
+       public void MyDerivedClassmethod()
+        {
+            a = 6;
+            b = 9;
+            c = 15;
+            d = 12;
+            e = 10;
+        }
+    }
+    public class MyOtherClass
+    {
+        /// <summary>
+        /// Accessing variables in other class in same library
+        /// </summary>
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass m1 = new MyBaseClass();
+            m1.a = 10;
+            m1.b = 20;
+            m1.c = 7;
+            m1.d = 15;
+            m1.e = 22;
+        }
+        
+       
+
+    }
+}
