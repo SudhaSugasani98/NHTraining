a88bdcb9ad568e289922f530a49459861a2ec8c9
SudhaSugasani <sudhasugasani@98.com>
2022-02-16 12:31:11 +0530

    Creating a layered architecture project with seperate class library for business requirements

---
+++ Day 18 Assignment/Day18Project1/Day18Project1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathematicsLibrary;
+
+namespace Day18Project1
+{
+    /***********************************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:By using the MathematicsLibrary creating console application.
+     * **********************************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The factorial value of {n} is {Algebra.Factorial(n)}");
+            Console.ReadLine();
+        }
+    }
+}
