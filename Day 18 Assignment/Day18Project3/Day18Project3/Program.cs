9b9cf5d9ed3e3f3e35363f74d5da209de5c92b10
SudhaSugasani <sudhasugasani@98.com>
2022-02-16 21:33:42 +0530

    Adding Palindrome method to existing TDD project

---
+++ Day 18 Assignment/Day18Project3/Day18Project3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathematicsLibrary2;
+
+namespace Day18Project3
+{
+    internal class Program
+    {
+        /***************************************************
+          * Author:Sudha Kumari Sugasani
+          * Purpose:Using MathematicsLibrary2 in consoleapp
+          * **************************************************/
+
+        static void Main(string[] args)
+        {
+            //Algebra palindrome = new Algebra();
+            int n;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial of {n} is {Algebra.Factorial(n)}");
+            int input;
+            Console.WriteLine("Enter a number");
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Algebra.isPalindromeorNot(input) ); 
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day 18 Assignment/Day18Project3/MathematicsLibrary2Tests/AlgebraTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathematicsLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsLibrary2.Tests
+{
+    [TestClass()]
+    public class AlgebraTests
+    {
+
+        /// <summary>
+        /// This method is used to test if the input is zero
+        /// </summary>
+        [TestMethod()]
+        public void FactorialTest_Zero_Input()
+        {
+            //Arrange
+            int n = 0;
+            int expected = 1;
+
+            //Act
+            int actual = Algebra.Factorial(n);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        /// <summary>
+        /// This method is used to test if the input is 1 to 7
+        /// </summary>
+        [TestMethod()]
+        public void FactorialTest_One_to_Seven_Input()
+        {
+            //Arrange
+            int n = 4;
+            int expected = 24;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// This method id used to test if the input is >7
+        /// </summary>
+        [TestMethod()]
+        public void FactorialTest_More_Than_Seven_Input()
+        {
+            //Arrange
+            int n = 9;
+            int expected = -999;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// This method is used to test if the input is <0
+        /// </summary>
+        [TestMethod()]
+        public void FactorialTest_Less_than_Zero_Input()
+        {
+            //Arrange
+            int n = -7;
+            int expected = -9999;
+            //Act
+            int actual = Algebra.Factorial(n);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// This method will check if the given number is palindrome or not
+        /// </summary>
+        [TestMethod()]
+        public void Palindrome_or_Not_Test()
+        {
+            //Arrange
+            int input = 121;
+            bool expected = true;
+            //Act
+            bool actual = Algebra.isPalindromeorNot(input);
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
