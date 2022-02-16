0afdbe772e030a93d207b9319bb12dc247f4d214
SudhaSugasani <sudhasugasani@98.com>
2022-02-16 17:01:39 +0530

    Creating tdd and layered architecture

---
+++ Day 18 Assignment/Day18Project1/Algebra.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsLibrary
+{
+    /*************************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Creating MathemematicsLibrary to reuse this in other
+     *         libraries or applications.
+     * ***********************************************************/
+    public class Algebra
+    {
+        /// <summary>
+        /// This method is used to get factorial of number
+        /// </summary>
+        /// <param name="n">int</param>
+        /// <returns>Factorial value(int)</returns>
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else if (n > 7)
+                return -999;
+            else if (n < 0)
+                return -9999;
+            else
+            {
+                int fact = 1;
+                for(int i=1;i<=n;i++)
+                {
+                    fact = fact * i;
+                }
+                return fact;
+            }
+        }
+    }
+}
---
+++ Day 18 Assignment/Day18Project2/Day18Project2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathematicsLibrary1;
+
+namespace Day18Project2
+{
+    /***************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Using MathematicsLibrary1 in consoleapp
+     * **************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial of {n} is {Algebra.Factorial(n)}");
+            Console.ReadLine();
+        }
+    }
+}
