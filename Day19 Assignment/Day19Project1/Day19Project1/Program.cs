bcede8aeb2fa6ee60279d67dc3e3577a1c7a0924
SudhaSugasani <sudhasugasani@98.com>
2022-02-17 12:11:35 +0530

    Program to read xml file using C#

---
+++ Day19 Assignment/Day19Project1/Day19Project1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day19Project1
+{
+    /*************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Program to read XML file using C# code.
+     * ************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+        
+            doc.Load("C:\\NH\\XML Files\\Employee.xml");
+            foreach(XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                string text = node.InnerText;
+                Console.WriteLine(text);
+               
+            }Console.ReadLine();
+        }  
+    }
+}
