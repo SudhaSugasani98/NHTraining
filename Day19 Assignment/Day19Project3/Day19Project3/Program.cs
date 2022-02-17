86e19acfc1ff754979afdcc37c00d8540b285ea9
SudhaSugasani <sudhasugasani@98.com>
2022-02-17 13:04:25 +0530

    Program to read data from xml file using C# and printing the values followed by ,

---
+++ Day19 Assignment/Day19Project3/Day19Project3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day19Project3
+{
+    /******************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Program to read XML file using C# code and 
+     *         printing details in XML file followed by ","
+     * ****************************************************/
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("C:\\NH\\XML Files\\Employee.xml");
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode cnode in node.ChildNodes)
+                {
+                    if (cnode.Name == "ID")
+                    {
+                        Console.Write(cnode.InnerText);
+                    }
+                    if(cnode.Name=="Name")
+                    {
+                        Console.Write(","+cnode.InnerText);
+                    }
+                    if(cnode.Name=="Salary")
+                    {
+                        Console.Write(","+cnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
