601ab088ec732db4b0be0d803b377e42561faf4c
SudhaSugasani <sudhasugasani@98.com>
2022-02-21 11:48:59 +0530

    Example program for Windows application and consume the web service

---
+++ Day 21 Assignment/Day21Project2/Day21Project2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Day21Project2.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day21Project2
+{
+    /******************************************************************************
+     * Author:Sudha Kumari Sugasani
+     * Purpose:Example program for windows application and consume the web service
+     *******************************************************************************/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            WebService1SoapClient obj = new WebService1SoapClient();
+            textBox2.Text = obj.Factorial(n).ToString();
+        }
+    }
+}
